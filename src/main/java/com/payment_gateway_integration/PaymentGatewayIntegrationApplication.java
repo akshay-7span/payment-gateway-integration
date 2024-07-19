649a440d44fb098d8c35794fb96ea9de17d9785e
krushna-7span <krushna@7span.com>
2024-07-19 14:35:13 +0530

    Initial commit

---
+++ src/main/java/com/payment_gateway_integration/PaymentGatewayIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.payment_gateway_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayIntegrationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/payment_gateway_integration/PaymentGatewayIntegrationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.payment_gateway_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
